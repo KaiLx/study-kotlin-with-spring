864a1c1dc3a8c384f6739c56ace294292bc19c12
gkasse <genki-kase@bm-sms.co.jp>
2017-05-09 12:17:53 +0900

    Add companion object, annotation of JvmStatic, able to run spring-boot in ide.

--- src/main/kotlin/com/github/kailx/study/StudyKotlinWithSpringApplication.kt
+++ src/main/kotlin/com/github/kailx/study/StudyKotlinWithSpringApplication.kt
@@ -4,8 +4,11 @@ import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 
 @SpringBootApplication
-class StudyKotlinWithSpringApplication
-
-fun main(args: Array<String>) {
-    SpringApplication.run(StudyKotlinWithSpringApplication::class.java, *args)
+class StudyKotlinWithSpringApplication {
+    companion object {
+        @JvmStatic
+        fun main(args: Array<String>) {
+            SpringApplication.run(StudyKotlinWithSpringApplication::class.java, *args)
+        }
+    }
 }
