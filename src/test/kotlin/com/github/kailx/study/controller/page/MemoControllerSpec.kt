9762c3ab176cc0ffaed7d2f8556d8b52fa0177d2
gkasse <genki-kase@bm-sms.co.jp>
2017-05-09 18:40:29 +0900

    Add RED test for post memo subject

--- src/test/kotlin/com/github/kailx/study/controller/page/MemoControllerSpec.kt
+++ src/test/kotlin/com/github/kailx/study/controller/page/MemoControllerSpec.kt
@@ -4,9 +4,11 @@ import com.github.kailx.study.model.Memo
 import org.jetbrains.spek.api.Spek
 import org.jetbrains.spek.api.dsl.describe
 import org.jetbrains.spek.api.dsl.it
-import org.junit.Assert
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers
+import org.jetbrains.spek.api.dsl.on
+import org.junit.Assert.assertNotNull
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
 import org.springframework.test.web.servlet.setup.MockMvcBuilders
 
 /**
@@ -18,26 +20,44 @@ class MemoControllerSpec : Spek({
     val mvc = MockMvcBuilders.standaloneSetup(MemoController()).build()
 
     describe("/memoにGETでアクセスした時") {
-        val result = mvc.perform(MockMvcRequestBuilders.get("/memo/"))
+        val result = mvc.perform(get("/memo/"))
 
         it("HTTPステータスコードが200で返却される") {
-            result.andExpect(MockMvcResultMatchers.status().isOk)
+            result.andExpect(status().isOk)
+        }
+    }
+
+    describe("/memo/subjectにGETでアクセスした時") {
+        val result = mvc.perform(get("/memo/subject"))
+
+        it("'subject'memoとして使われる") {
+            TODO()
+        }
+        on("authorがクエリパラメータで与えられなかった場合") {
+            it("'Default Author'がauthorとして使われる") {
+                TODO()
+            }
+        }
+        on("authorがクエリパラメータで与えられた場合") {
+            val author = "author"
+            it("'$author'がauthorとして使われる") {
+                TODO()
+            }
         }
     }
 
     describe("/memoにPOSTでアクセスした時") {
         val result = mvc.perform(
-                MockMvcRequestBuilders
-                        .post("/memo/")
+                post("/memo/")
                         .param("memo", "memo")
                         .param("author", "author")
         )
 
         it("HTTPステータスコードが200で返却される") {
-            result.andExpect(MockMvcResultMatchers.status().isOk)
+            result.andExpect(status().isOk)
         }
         it ("期待通りの値がMemoオブジェクトに格納される") {
-            Assert.assertNotNull(result.andReturn().modelAndView.modelMap["items"])
+            assertNotNull(result.andReturn().modelAndView.modelMap["items"])
 
             val items = result.andReturn().modelAndView.modelMap["items"] as List<*>
             assert(items.size == 1)
