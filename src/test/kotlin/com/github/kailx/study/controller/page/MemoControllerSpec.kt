e7b72c750fd51399197476ff8987d6e1b88835f2
gkasse <genki-kase@bm-sms.co.jp>
2017-05-09 17:09:18 +0900

    Add test when parameter posted

--- src/test/kotlin/com/github/kailx/study/controller/page/MemoControllerSpec.kt
+++ src/test/kotlin/com/github/kailx/study/controller/page/MemoControllerSpec.kt
@@ -1,8 +1,10 @@
 package com.github.kailx.study.controller.page
 
+import com.github.kailx.study.model.Memo
 import org.jetbrains.spek.api.Spek
 import org.jetbrains.spek.api.dsl.describe
 import org.jetbrains.spek.api.dsl.it
+import org.junit.Assert
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers
 import org.springframework.test.web.servlet.setup.MockMvcBuilders
@@ -20,6 +22,29 @@ class MemoControllerSpec : Spek({
 
         it("HTTPステータスコードが200で返却される") {
             result.andExpect(MockMvcResultMatchers.status().isOk)
+        }
+    }
+
+    describe("/memoにPOSTでアクセスした時") {
+        val result = mvc.perform(
+                MockMvcRequestBuilders
+                        .post("/memo/")
+                        .param("memo", "memo")
+                        .param("author", "author")
+        )
+
+        it("HTTPステータスコードが200で返却される") {
+            result.andExpect(MockMvcResultMatchers.status().isOk)
+        }
+        it ("期待通りの値がMemoオブジェクトに格納される") {
+            Assert.assertNotNull(result.andReturn().modelAndView.modelMap["items"])
+
+            val items = result.andReturn().modelAndView.modelMap["items"] as List<*>
+            assert(items.size == 1)
+            
+            val item = items[0] as Memo
+            assert(item.memo == "memo")
+            assert(item.author == "author")
         }
     }
 })
