019c5e0abd6707dd250f29da7f4e558a87f81b84
gkasse <genki-kase@bm-sms.co.jp>
2017-05-11 16:52:16 +0900

    Add repository injection

--- src/main/kotlin/com/github/kailx/study/service/MemoService.kt
+++ src/main/kotlin/com/github/kailx/study/service/MemoService.kt
@@ -1,6 +1,7 @@
 package com.github.kailx.study.service
 
 import com.github.kailx.study.model.Memo
+import com.github.kailx.study.repository.MemoRepository
 import org.springframework.stereotype.Service
 import java.util.*
 
@@ -9,6 +10,12 @@ import java.util.*
  * @author KaiLx
  */
 @Service
-class MemoService {
+class MemoService(val memoRepository: MemoRepository) {
     fun join(memo: String, author: String): Memo = Memo(memo, author, Date())
+
+    fun readAll(): List<Memo> = memoRepository.find()
+
+    fun readByAuthor(author: String): List<Memo> = memoRepository.findByAuthor(author)
+
+    fun write(memo: String, author: String) = memoRepository.save(Memo(memo, author, Date()))
 }
